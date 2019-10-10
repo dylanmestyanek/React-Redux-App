1bbb56c75e9b8ca959d5ac629ca9607de0581223
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-10 15:04:02 -0500

    Dependencies installed. Redux store setup complete, along with movieListReducer.

---
+++ studio-ghibli-react-redux/src/reducers/movieListReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    movies: []
+}
+
+export const movieListReducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
